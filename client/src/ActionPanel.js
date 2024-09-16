81dc25332b2754a2322893b7cafc392c911698d3
parkerspeares <parker.speares@outlook.com>
2024-09-16 15:22:16 -0700

    Action Panel JS
    the action panel for the UI

---
+++ client/src/ActionPanel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ActionPanel = () => {
+  return (
+    <div className="action-panel">
+      <button>Attack</button>
+      <button>Defend</button>
+      <button>Cast Spell</button>
+    </div>
+  );
+};
+
+export default ActionPanel;
