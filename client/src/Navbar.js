589e063156c3d46c282d5dea1af8e27ef7be1c7e
parkerspeares <parker.speares@outlook.com>
2024-09-16 15:18:13 -0700

    Navbar JS
    Navigation bar for UI

---
+++ client/src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Character</li>
+        <li>Inventory</li>
+        <li>Quests</li>
+        <li>Map</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
