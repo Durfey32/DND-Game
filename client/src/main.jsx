af3b9980af675403e62a60cab6675bbdb36912b0
parkerspeares <parker.speares@outlook.com>
2024-09-16 19:45:50 -0700

    updated ActionPanel & Navar files
    needed jsx files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Navbar from './components/Navbar.js';
-import ActionPanel from './components/ActionPanel';
+import ActionPanel from './components/ActionPanel.js';
+import GameMap from './components/GameMap.js';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
