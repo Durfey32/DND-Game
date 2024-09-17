75920960fca08cee28836d629df39d7d83751d04
parkerspeares <parker.speares@outlook.com>
2024-09-16 19:55:36 -0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Navbar from './components/Navbar.js';
-import ActionPanel from './components/ActionPanel.js';
-import GameMap from './components/GameMap.js';
+import Navbar from './components/Navbar';
+import ActionPanel from './components/ActionPanel';
+import GameMap from './components/GameMap';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="game-container">
+        <GameMap />
+        <ActionPanel />
+      </div>
+    </div>
+  );
+}
