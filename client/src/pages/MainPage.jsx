eb70d0cf166510133515c9a56812b45a950ed641
Your Name <kboothe617@gmail.com>
2024-09-17 20:06:29 -0600

    started mainpage function

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -3,3 +3,12 @@ import Navbar from "./components/Navbar";
 import ActionPanel from "./components/ActionPanel";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+function MainPage() {
+    return (
+        <Router>
+            <div className="main-page">
+            </div>
+        </Router>
+    );
+}
+export default MainPage;
