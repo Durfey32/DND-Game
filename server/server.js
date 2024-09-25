daccc957f0fa97ae60a04dbd1222fd1e2181001b
Spencer_Durfey <durfey_32@yahoo.com>
2024-09-24 18:45:16 -0600

    connect to postgresql

--- server/server.js
+++ server/server.js
@@ -3,8 +3,33 @@ import sequelize from './src/config/connection.js';
 import routes from './src/routes/index.js';
 import dotenv from 'dotenv';
 import authRoutes from './src/routes/auth-routes.js';
+import { Sequelize } from 'sequelize';
 
 dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+  },);
+
+  let sequelize;
+  if (process.env.DB_URL) {
+    sequelize = new Sequelize(process.env.DB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PW,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+      },
+    );
+  }
 
 const app = express();
 
