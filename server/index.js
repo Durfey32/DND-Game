e18703d1d84bdc24cbde4fe7b4aaf0c126bd4f4e
Spencer_Durfey <durfey_32@yahoo.com>
2024-09-26 23:07:15 -0600

    updated package.json

--- server/index.js
+++ server/index.js
@@ -20,5 +20,5 @@ sequelize.sync({ force: forceDatabaseSync }).then(() => {
 
 });
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+// <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
